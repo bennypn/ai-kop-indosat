11d82f5ad124d85a4ac2657f098dcbc7528ced1b
Ben <bennyperdana.jakarta@gmail.com>
2025-06-26 02:33:13 +0700

    Update database configuration with new user credentials for improved security

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ DB_CONFIG = {
     "host": "localhost",
     "port": 5432,
     "database": "ai-core",
-    "user": "postgres",
-    "password": "postgres",
+    "user": "user_admin",
+    "password": "Testing1",
 }
 
 MAX_THREADS = 3
